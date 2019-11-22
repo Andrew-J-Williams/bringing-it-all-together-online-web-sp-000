36a4227926faef57b12f65ebea09b3ab3ec8fd4b
Andrew Williams <1williamsaj@gmail.com>
2019-11-22 02:47:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,9 +28,9 @@ class Dog
 
   def self.new_from_db(row)
     id = row[0]
-    name: = row[1]
-    grade: = row[2]
-    self.new(id, name:, grade:)
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
   end
 
 
