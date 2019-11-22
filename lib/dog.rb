e8e30c03e6f05922b862266b9e74681a58e78276
Andrew Williams <1williamsaj@gmail.com>
2019-11-22 03:11:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -81,7 +81,10 @@ class Dog
   end
 
   def update
-    
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
   end
 
 end
