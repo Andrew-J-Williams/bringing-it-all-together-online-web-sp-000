7ce9824006481826da0abf17c294a86787d1990a
Andrew Williams <1williamsaj@gmail.com>
2019-11-22 03:01:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,6 +55,16 @@ class Dog
   end
 
   def self.find_by_name
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
 
   end
 
