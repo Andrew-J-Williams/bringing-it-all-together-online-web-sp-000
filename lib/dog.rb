fbcde893fa3ec540444b6f6cff78c55a943c6a0a
Andrew Williams <1williamsaj@gmail.com>
2019-11-22 03:22:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -93,7 +93,7 @@ class Dog
       if dog
         dog_data = self.new_from_db(dog)
       else
-        new_dog = self.create({:name => name, :breed => breed})
+        dog_data = self.create({:name => name, :breed => breed})
       end
       new_dog
   end
